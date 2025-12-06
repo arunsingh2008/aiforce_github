d269e2074eab15c735fc9d838aaa0be8d7326051
arunsingh2008 <74750738+arunsingh2008@users.noreply.github.com>
2025-12-06 11:34:33 +0530

    Add snowflake_sql_test.sql

---
+++ snowflake_sql_test.sql
@@ -0,0 +1,86 @@
+WITH
+-- CTE for remdup operation
+RDU_Highest_Facility AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY SRC_DL, AR_ID ORDER BY (SELECT NULL)) AS row_num
+    FROM
+        V438S0P1
+    QUALIFY row_num = 1
+),
+
+-- CTE for join operation
+JNO_FCY_AR_LWST_HGHST AS (
+    SELECT
+        a.*,
+        b.*
+    FROM
+        V438S1P1 a
+    LEFT JOIN
+        V438S1P2 b
+    ON
+        a.SRC_DL = b.SRC_DL AND a.AR_ID = b.AR_ID
+),
+
+-- CTE for transformation operation
+XFM_HIGHEST_FACILITY AS (
+    SELECT
+        mov_AR_X_R_HIGHEST.*,
+        mov_AR_X_R_HIGHEST.HIGHEST_FCY_ID AS AR_ID,
+        'Y' AS HIGHEST_FCY_IN_HRY
+    FROM
+        V438S2P1 mov_AR_X_R_HIGHEST
+),
+
+-- CTE for sort operation
+SRT_AR_X_R_HIGHEST AS (
+    SELECT
+        *
+    FROM
+        V438S3P1
+    ORDER BY
+        SRC_DL, AR_ID
+)
+
+-- Final SELECT statement
+SELECT
+    DATA_DT,
+    OBJ_AR_ID AS FCY_ID,
+    MSTR_SRC_STM_CD AS SRC_STM_ID,
+    mov_FCY_AR_IDENTN.SYS_VLD_FROM_TMS AS DATE_FROM,
+    AR_ID,
+    VLD_TO_TMS AS DATE_TO,
+    SRC_DL,
+    FCY_RK,
+    FCY_AR_TP,
+    CASE
+        WHEN IsNull(mov_FCY_AR_IDENTN.HIGHER_FCY_RK) THEN mov_FCY_AR_IDENTN.FCY_RK
+        ELSE mov_FCY_AR_IDENTN.HIGHER_FCY_RK
+    END AS HIGHER_FCY_RK,
+    CASE
+        WHEN IsNull(mov_FCY_AR_IDENTN.HIGHEST_FCY_RK) THEN mov_FCY_AR_IDENTN.FCY_RK
+        ELSE mov_FCY_AR_IDENTN.HIGHEST_FCY_RK
+    END AS HIGHEST_FCY_RK,
+    CASE
+        WHEN mov_FCY_AR_IDENTN.HLEAF = 1 THEN 'Y'
+        WHEN mov_FCY_AR_IDENTN.HLEAF = 0 THEN 'N'
+        WHEN mov_FCY_AR_IDENTN.HIGHEST_FCY_IN_HRY = 'Y' THEN 'N'
+        ELSE 'Y'
+    END AS LOWEST_LVL_IND,
+    FCY_RK AS SPSDG_FCY_RK,
+    COURT_CTRLD_WRKOUT_FILL_DT,
+    COURT_CTRLD_WRKOUT_FCY,
+    OUT_OF_COURT_WRKOUT_FCY,
+    mov_FCY_AR_IDENTN.SPSDG_FCY_DT AS SPSDG_FCY_DT,
+    COURT_CTRLD_WRKOUT_CLS_DT,
+    CR_OBLG_DFLTD,
+    mov_FCY_AR_IDENTN.AR_IDENTN_NM AS FCY_VORTEX_ID,
+    CURRENT_TIMESTAMP() AS SYS_INRT_TMS
+FROM
+    RDU_Highest_Facility
+JOIN
+    JNO_FCY_AR_LWST_HGHST ON RDU_Highest_Facility.SRC_DL = JNO_FCY_AR_LWST_HGHST.SRC_DL AND RDU_Highest_Facility.AR_ID = JNO_FCY_AR_LWST_HGHST.AR_ID
+JOIN
+    XFM_HIGHEST_FACILITY ON RDU_Highest_Facility.SRC_DL = XFM_HIGHEST_FACILITY.SRC_DL AND RDU_Highest_Facility.AR_ID = XFM_HIGHEST_FACILITY.AR_ID
+JOIN
+    SRT_AR_X_R_HIGHEST ON RDU_Highest_Facility.SRC_DL = SRT_AR_X_R_HIGHEST.SRC_DL AND RDU_Highest_Facility.AR_ID = SRT_AR_X_R_HIGHEST.AR_ID
